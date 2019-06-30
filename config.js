70689ea6617f268a4eef376defa14e72fd2f865f
Sebastian Basiński <basinski.sebastian@gmail.com>
2019-06-30 22:40:38 +0200

    Add basic files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '347508140197-g1o9ma0043sditepnecaka1i0lpomadh.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'pC0Cgp76FbG5dNk6ZPtAYRZK',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
